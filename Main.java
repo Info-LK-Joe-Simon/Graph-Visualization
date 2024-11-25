2a51f6c1e8000eb246ef5f72294bf9150be488bc
Fabian-MeinOpaNenntMichFlorian-OderPaul <78973012+Fabian-MeinOpaNenntMichFlorian-OderPaul@users.noreply.github.com>
2024-11-25 21:35:00 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        //Create our graph
+      
+        Display d=new Display(800, 800, /*Adjacency matrix*/);
+        //d.setFillKnots(true);
+        //d.setPrintWeight(false);
+    }
+}
