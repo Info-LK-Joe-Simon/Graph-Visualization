49e43cf1145098beac7f459f35e1e9507be5e2a9
Fabian-MeinOpaNenntMichFlorian-OderPaul <78973012+Fabian-MeinOpaNenntMichFlorian-OderPaul@users.noreply.github.com>
2024-12-06 17:52:46 +0100

    Update Main.java
    AdjMat in Main mit double

--- Main.java
+++ Main.java
@@ -2,11 +2,12 @@ public class Main {
     public static void main(String[] args) {
       
         //Create our graph
-        int[][] adjMat = new int[10][10];
+        double[][] adjMat = new double[10][10];
         for (int i=0; i< adjMat.length; i++){
             for (int j=0; j< adjMat[i].length; j++) {
-                adjMat[i][j]=(Math.random()<0.5)?0:(int)(Math.random()*20);
-                System.out.println(i+" "+j+" "+adjMat[i][j]);
+                if (i==j)
+                    continue;
+                adjMat[i][j]=(Math.random()<0.5)?0:(Math.random()*20);
             }
         }
       
