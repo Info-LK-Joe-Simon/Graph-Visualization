1b12f913624b947d944d27431f2ef1eeea76dbf3
Fabian-MeinOpaNenntMichFlorian-OderPaul <78973012+Fabian-MeinOpaNenntMichFlorian-OderPaul@users.noreply.github.com>
2024-12-06 18:35:38 +0100

    Update Main.java
    Example for renaming knot

--- Main.java
+++ Main.java
@@ -7,11 +7,19 @@ public class Main {
             for (int j=0; j< adjMat[i].length; j++) {
                 if (i==j)
                     continue;
-                adjMat[i][j]=(Math.random()<0.5)?0:(Math.random()*20);
+                adjMat[i][j]=(Math.random()<0.8)?0:(Math.random()*20);
             }
         }
-      
-        Display d=new Display(800, 800, AdjMat/*Adjacency matrix*/);
+        Display d=new Display(800, 800, adjMat/*g.getAdjMatrix()*/);
+        
+        //Example for renaming
+        try {
+            Thread.sleep(100);    //Ensure the thread has initialized properly before proceeding
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        d.changeKnotenName(0, "Example");
+        
         //d.setFillKnots(true);
         //d.setPrintWeight(false);
     }
